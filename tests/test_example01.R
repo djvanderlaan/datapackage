b980cb8488d83707842c324ffb7ad11f90a60fc1
djvanderlaan <jan@ntteloos.nl>
2023-11-30 08:03:23 +0100

    Added tests for empty resource

--- tests/test_example01.R
+++ tests/test_example01.R
@@ -9,7 +9,7 @@ dp <- opendatapackage(dir)
 expect_equal(dpname(dp), "test01")
 expect_equal(dptitle(dp), "Test datapackage 01")
 expect_equal(dpdescription(dp), "A data package to test if tooling functions correctly")
-expect_equal(dpresourcenames(dp), c("complex", "codelist-factor1", 
+expect_equal(dpresourcenames(dp), c("complex", "complex-empty", "codelist-factor1", 
     "codelist-factor2", "inline", "fixed-width"))
 
 res <- dpresource(dp, "complex")
@@ -45,6 +45,32 @@ expect_equal(dta$factor1,
 stopifnot(!is.null(attr(dta$factor1, "fielddescriptor")))
 expect_equal(dta$factor2, 
   factor(c(1,2,1,3,2,NA), levels=1:3, labels=c("circle", "square", "triangle")),
+  attributes = FALSE
+)
+stopifnot(!is.null(attr(dta$factor2, "fielddescriptor")))
+
+
+# =============================================================================
+# Test the empty dataset
+dta <- dp |> dpresource("complex-empty") |> dpgetdata()
+expect_equal(class(dta), "data.frame")
+expect_equal(nrow(dta), 0)
+expect_equal(sapply(dta, class), 
+  c(string1 = "character", integer1 = "integer", boolean1 = "logical", 
+  number1 = "numeric", number2 = "numeric", boolean2 = "logical", 
+  date1 = "Date", factor1 = "integer", factor2 = "character")
+)
+
+dta <- dp |> dpresource("complex-empty") |> dpgetdata(to_factor = TRUE)
+# We expect that other fields than the factor fields are ok; we tested that
+# previously. Here test only the factor variables
+expect_equal(dta$factor1, 
+  factor(integer(0), levels=1:4, labels=c("Purple", "Red", "Other", "Not given")),
+  attributes = FALSE
+)
+stopifnot(!is.null(attr(dta$factor1, "fielddescriptor")))
+expect_equal(dta$factor2, 
+  factor(integer(0), levels=1:3, labels=c("circle", "square", "triangle")),
   attributes = FALSE
 )
 stopifnot(!is.null(attr(dta$factor2, "fielddescriptor")))
