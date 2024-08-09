740ac63aca5ce3258dc479cbcf435ab6a1fdaa06
djvanderlaan <jan@ntteloos.nl>
2024-08-09 08:19:57 +0200

    New categories field

--- R/dpcodelist.R
+++ R/dpcodelist.R
@@ -26,23 +26,64 @@ dpcodelist.default <- function(x, fielddescriptor = attr(x, "fielddescriptor"),
   if (!is.null(fielddescriptor)) res <- dpcodelist(fielddescriptor)
   if (is.null(res) && is.factor(x)) {
     res <- data.frame(
-      code = seq_len(nlevels(x)),
+      value = seq_len(nlevels(x)),
       label = levels(x)
     )
   }
   res
 }
 
+## @rdname dpcodelist
+## @export
+#dpcodelist.fielddescriptor <- function(x, datapackage = dpgetdatapackage(x), ...) {
+#  codelist <- x$codelist
+#  if (is.null(codelist)) return(NULL)
+#  if (is.character(codelist)) {
+#    stopifnot(is.character(codelist), length(codelist) == 1)
+#    codelist <- dpgetdata(datapackage, codelist)
+#  }
+#  stopifnot(is.data.frame(codelist))
+#  codelist
+#}
+
 #' @rdname dpcodelist
 #' @export
 dpcodelist.fielddescriptor <- function(x, datapackage = dpgetdatapackage(x), ...) {
-  codelist <- x$codelist
-  if (is.null(codelist)) return(NULL)
-  if (is.character(codelist)) {
-    stopifnot(is.character(codelist), length(codelist) == 1)
-    codelist <- dpgetdata(datapackage, codelist)
+  codelist <- x$categories
+  if (is.null(codelist)) {
+    return(NULL)
+  } else if (is.data.frame(codelist)) {
+    stop("Unsupported format for categories: 'data.frame'.")
+  } else if (is.list(codelist)) {
+    if (is.null(names(codelist))) {
+      # We have a list with labels and values
+      values <- sapply(codelist, \(x) x$value)
+      labels <- sapply(codelist, \(x) 
+        if (hasName(x, "label")) x$label else x$value)
+      codelist <- data.frame(value = values, label = labels)
+    } else {
+      # We have a reference to a data resource
+      if (!hasName(codelist, "resource")) 
+        stop("'resource' field missing from categories.")
+      resource <- codelist$resource
+      categories <- codelist
+      codelist <- dpgetdata(datapackage, resource)
+      if (hasName(categories, "valueField")) 
+        names(codelist)[names(codelist) == categories$valueField] <- "value"
+      if (hasName(categories, "labelField")) 
+        names(codelist)[names(codelist) == categories$labelField] <- "label"
+    }
+  } else if (is.numeric(codelist) || is.character(codelist) || is.logical(codelist)) {
+    # We have a vector with categories
+    codelist <- data.frame(value = codelist, label = codelist)
+  } else {
+    stop("Unsupported format for categories: '", 
+      paste0(class(codelist), collapse ="', '"), "'.")
   }
+  # Check of output seems ok
   stopifnot(is.data.frame(codelist))
+  if (!("value" %in% names(codelist))) stop("'value' column missing from codelist.")
+  if (!("label" %in% names(codelist))) stop("'label' column missing from codelist.")
   codelist
 }
 
