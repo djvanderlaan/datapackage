a16d1bd787137dfad5e272aac19041bc502edb78
Jan van der Laan <jan@ntteloos.nl>
2025-01-12 10:40:04 +0100

    coded

--- work/coded.R
+++ work/coded.R
@@ -1,29 +1,9 @@
 library(datapackage)
-library(codelist)
 
-fn <- system.file("examples/iris", package = "datapackage")
-dp <- opendatapackage(fn)
-
-dp
-
-
-dta <- dp |> dpgetdata("complex", convert_categories = "to_factor")
-dta
-
-dta <- dp |> dpgetdata("complex", convert_categories = "no")
-dta
-
-dpcategorieslist(dta$factor1)
-
-cl <- dpcategorieslist(dta$factor1) |> as.codelist()
-cl
-
-tmp <- coded(dta$factor1, codelist = cl)
-tmp
-
-dptofactor
-
-dptocoded <- function(x, categorieslist = dpcategorieslist(x), ..., warn = FALSE) {
+dp_to_coded <- function(x, categorieslist = dp_categorieslist(x), ..., warn = FALSE) {
+  if (!requireNamespace("codelist")) {
+    stop("In order to use 'dp_to_coded' the 'codelist' package needs to be installed.")
+  }
   if (is.null(categorieslist)) {
     if (warn) warning("Field does not have an associated code list. Returning original vector.")
     return(x)
@@ -32,13 +12,25 @@ dptocoded <- function(x, categorieslist = dpcategorieslist(x), ..., warn = FALSE
   # Determine which columns from the categorieslist contain the 
   # codes and labels
   fields <- datapackage:::getfieldsofcategorieslist(categorieslist)
-  codelist <- as.codelist(categorieslist, code = fields$value, label = fields$label, ...)
-  coded(x, codelist = codelist)
+  print(fields)
+  codelist <- codelist::as.codelist(categorieslist, code = fields$value, label = fields$label, ...)
+  codelist::coded(x, codelist = codelist)
 }
 
-dta <- dp |> dpresource("complex") |> dpgetdata(convert_categories = "dptocoded")
+fn <- system.file("examples/iris", package = "datapackage")
+dp <- open_datapackage(fn)
+dp
 
+
+dta <- dp |> dp_get_data("complex", convert_categories = "to_factor")
 dta
+
+dta <- dp |> dp_get_data("complex", convert_categories = "no")
+dta
+
+dta <- dp |> dp_get_data("complex", convert_categories = "dp_to_coded")
+dta
+
 
 dta$factor1 == 1
 dta$factor1 == 10
@@ -46,23 +38,12 @@ dta$factor1 == "Purple"
 dta$factor1 == as.label("Purple")
 
 
-dta <- dploadfromdatapackage("work/employ", convert_categories = "dptocoded")
-
+dta <- dp_load_from_datapackage("work/employ", 
+  convert_categories = "dp_to_coded")
+dta
 
 table(labels(dta$employmentstatus, FALSE), useNA = "ifany")
 
-
-cl <- dpcategorieslist(dta$employmentstatus)
-cl
-
-as.codelist(cl, format = "wide")
-
-
-dptocoded(dta$factor1)
-
-for (col in names(dta)) dta[[col]] <- dptocoded(dta[[col]], warn = FALSE)
-
-dta
-
+table(labels(dta$employmentstatus), useNA = "ifany")
 
 
