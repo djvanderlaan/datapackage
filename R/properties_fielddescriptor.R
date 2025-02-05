71e2eac56959f71bae6e4d0cdeee058aa0aeb108
Jan van der Laan <jan@ntteloos.nl>
2025-02-05 14:57:33 +0100

    Added some setters and getters for field descriptor

---
+++ R/properties_fielddescriptor.R
@@ -0,0 +1,110 @@
+
+# ==============================================================================
+# NAME
+# Required; string; onl lower case alnum and _/-/.
+
+#' Getting and setting properties of Data Resources
+#'
+#' @param x a \code{fielddescriptor} object.
+#' 
+#' @param value the new value of the property.
+#'
+#' @param ... used to pass additional arguments to other methods.
+#'
+#' @return
+#' Either returns the property or modifies the object. If the property is not
+#' set \code{NULL} is returned (unless \code{default = TRUE}).
+#' 
+#' @export
+#' @rdname properties_fielddescriptor
+dp_name.fielddescriptor <- function(x) {
+  res <- dp_property(x, "name")
+  # Name is required for data resource
+  if (is.null(res))
+    stop("Required attribute 'name' is missing from Data Resource.")
+  res
+}
+
+#' @export
+#' @rdname properties_dataresource
+`dp_name<-.fielddescriptor` <- function(x, value) {
+  value <- paste0(value)
+  if (!isname(value)) stop("name should consists only of lower case letters, ",
+      "numbers, '-', '.' or '_'.")
+  dp_property(x, "name") <- value
+  x
+}
+
+# ==============================================================================
+# TITLE
+# Optional; string
+
+#' @export
+#' @rdname properties_fielddescriptor
+dp_title.fielddescriptor <- function(x) {
+  dp_property(x, "title")
+}
+
+#' @export
+#' @rdname properties_dataresource
+`dp_title<-.fielddescriptor` <- function(x, value) {
+  if (!is.null(value)) {
+    value <- paste0(value)
+    if (!isstring(value)) stop("value should be a character of length 1.")
+  }
+  dp_property(x, "title") <- value
+  x
+}
+
+# ==============================================================================
+# DESCRIPTION
+# Optional; string
+
+#' @param firstparagraph Only return the first paragraph of the description.
+#' 
+#' @param dots When returning only the first paragraph indicate missing
+#' paragraphs with \code{...}.
+#'
+#' @export
+#' @rdname properties_dataresource
+dp_description.fielddescriptor <- function(x, ..., firstparagraph = FALSE, 
+    dots = FALSE) {
+  res <- dp_property(x, "description")
+  if (!is.null(res) && firstparagraph) getfirstparagraph(res, dots) else res
+}
+
+#' @export
+#' @rdname properties_fielddescriptor
+`dp_description<-.fielddescriptor` <- function(x, value) {
+  if (!is.null(value)) {
+    value <- paste0(value, collapse = "\n")
+    # Because of the paste0 above value will always be a string
+  }
+  dp_property(x, "description") <- value
+  x
+}
+
+
+# ==============================================================================
+# FORMAT
+# Optional; string
+
+#' @export
+#' @rdname properties_fielddescriptor
+dp_format.fielddescriptor <- function(x, ...) {
+  dp_property(x, "format")
+}
+
+#' @export
+#' @rdname properties_dataresource
+`dp_format<-.fielddescriptor` <- function(x, value) {
+  if (!is.null(value)) {
+    if (!is.character(value) && length(value) == 1)
+      stop("format should be a character of length 1.")
+    # TODO: accepted values depend on type
+  }
+  dp_property(x, "format") <- value
+  x
+}
+
+
---
+++ tests/properties_fielddescriptor.R
@@ -0,0 +1,47 @@
+library(datapackage)
+
+source("helpers.R")
+
+f <- structure(list(
+    name = "foo", title = "Foo", description = "Foo Bar\n\nFoo", 
+    format = "bar"), class = "fielddescriptor")
+expect_equal(dp_name(f), "foo")
+expect_equal(dp_title(f), "Foo")
+expect_equal(dp_description(f), "Foo Bar\n\nFoo")
+expect_equal(dp_description(f, firstparagraph = TRUE), "Foo Bar")
+expect_equal(dp_format(f), "bar")
+
+# No properties set
+f <- structure(list(), class = "fielddescriptor")
+expect_error(dp_name(f))
+expect_equal(dp_title(f), NULL)
+expect_equal(dp_description(f), NULL)
+expect_equal(dp_description(f, firstparagraph = TRUE), NULL)
+expect_equal(dp_format(f), NULL)
+
+# Setting properties
+f <- structure(list(), class = "fielddescriptor")
+
+dp_name(f) <- "foo"
+expect_equal(dp_name(f), "foo")
+expect_error(dp_name(f) <- "")
+expect_error(dp_name(f) <- letters[1:2])
+
+dp_title(f) <- "Foo"
+expect_equal(dp_title(f), "Foo")
+expect_error(dp_title(f) <- c("a", "B"))
+dp_title(f) <- NULL
+expect_equal(dp_title(f), NULL)
+
+dp_description(f) <- "Foo Bar\n\nFoo"
+expect_equal(dp_description(f), "Foo Bar\n\nFoo")
+dp_description(f) <- c("Foo", "Bar")
+expect_equal(dp_description(f), "Foo\nBar")
+dp_description(f) <- NULL
+expect_equal(dp_description(f), NULL)
+
+dp_format(f) <- "bar"
+expect_equal(dp_format(f), "bar")
+dp_format(f) <- NULL
+expect_equal(dp_format(f), NULL)
+
